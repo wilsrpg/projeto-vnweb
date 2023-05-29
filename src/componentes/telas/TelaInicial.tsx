0dddffec690e24bdd6da7597a202525009eae34b
wilsrpg <wilson.rpg@hotmail.com>
2023-05-29 01:35:57 -0300

    passei botão de importação de roteiro pra tela inicial, removi alguns comentários

--- src/componentes/telas/TelaInicial.tsx
+++ src/componentes/telas/TelaInicial.tsx
@@ -2,14 +2,17 @@ import { useContext, useEffect, useState } from "react";
 import { acoes } from "../sistema/Redutor";
 import { contexto } from "../sistema/Contexto";
 import { imagensDeFundo } from "../mapeadores/ImagensDeFundo";
+import { personagens } from "../mapeadores/Personagens";
 import { musicas } from "../mapeadores/Musicas";
 import { sons } from "../mapeadores/Sons";
 import Botao from "../interface/Botao";
+import { roteiros } from "../roteiros/ListaDeRoteiros";
 
 export default function TelaInicial() {
   const sistema = useContext(contexto);
-  //const [anexou, anexar] = useState(false);
   let menuInicial = document.getElementById("menu");
+  const leitorDeArquivos = new FileReader();
+  const [arqRoteiro,definirArqRoteiro] = useState<File>();
   if(sistema?.estado.msgsConsole.renderizacoes)
     console.log("renderizou tela inicial");
   
@@ -22,7 +25,6 @@ export default function TelaInicial() {
     sistema?.despachar({tipo: acoes.aceitarInteracao, opcao: false});
     sistema?.despachar({tipo: acoes.mudarRoteiro, numero1: -1});
     sistema?.despachar({tipo: acoes.mudarEvento, numero1: -1});
-    //anexar(true);
     menuInicial = document.getElementById("menu");
     new Promise((resolve) => {setTimeout(() => {resolve("");}, 1000);})
     .then(()=>{
@@ -31,53 +33,40 @@ export default function TelaInicial() {
     })
   }, [])
 
-  /*useEffect(()=>{
-    if(anexou){
-      while(menuInicial?.childElementCount)
-        menuInicial.firstChild?.remove();
-      anexarMenu();
-    }
-  }, [anexou]);
-
-  async function anexarMenu(){
-    await new Promise((resolve) => setTimeout(() => resolve(""), 1000));
+  useEffect(()=>{
     if(sistema?.estado.msgsConsole.effects)
-      console.log("ef telainicial, entrou função anexarmenu");
-    menuInicial = document.getElementById("menu");
-    
-    const botaoNovoJogo = criarBotaoDeMenu("Novo Jogo",menuNovoJogo);
-    const botaoContinuar = criarBotaoDeMenu("Continuar",menuContinuar);
-    const botaoOpcoes = criarBotaoDeMenu("Opções",menuOpcoes);
-    menuInicial?.appendChild(botaoNovoJogo);
-    menuInicial?.appendChild(botaoContinuar);
-    menuInicial?.appendChild(botaoOpcoes);
-    botaoNovoJogo.focus();
-    //const botaoSom = document.getElementById("botão de som");
+      console.log("ef arqRoteiro="+arqRoteiro);
+    if(arqRoteiro){
+      leitorDeArquivos.readAsText(arqRoteiro);
+      new Promise((r)=>leitorDeArquivos.addEventListener("load",r))
+      .then(importarRoteiro);
+    }
+  }, [arqRoteiro]);
 
-    botaoNovoJogo?.addEventListener("keydown",(e)=>{
-      if(e.key == "ArrowUp") botaoOpcoes.focus();
-      if(e.key == "ArrowDown") botaoContinuar.focus();
-      //if(e.key == "ArrowRight" && botaoSom) botaoSom.focus();
-    });
-    botaoContinuar?.addEventListener("keydown",(e)=>{
-      if(e.key == "ArrowUp") botaoNovoJogo.focus();
-      if(e.key == "ArrowDown") botaoOpcoes.focus();
-      //if(e.key == "ArrowRight" && botaoSom) botaoSom.focus();
-    });
-    botaoOpcoes?.addEventListener("keydown",(e)=>{
-      if(e.key == "ArrowUp") botaoContinuar.focus();
-      if(e.key == "ArrowDown") botaoNovoJogo.focus();
-      //if(e.key == "ArrowRight" && botaoSom) botaoSom.focus();
-    });
+  function importarRoteiro() {
+    if(arqRoteiro?.type == "application/json"){
+      if(typeof leitorDeArquivos.result == "string"){
+        let roteiroJson = JSON.parse(leitorDeArquivos.result,(chave,valor)=>{
+          if(chave == "mudarCenario" || chave == "endereco" || chave == "mudarMusica" && typeof valor == "string"
+              || chave == "tocarSom" && typeof valor == "string")
+            valor = obterEndereco(valor);
+          return valor;
+        });
+        console.log("Roteiro JSON para teste carregado.");
+        console.log(roteiroJson);
+        roteiros[0] = roteiroJson.roteiro;
+        menuTestarRoteiro();
+      }
+    }
   }
 
-  function criarBotaoDeMenu(desc: string, func: ()=>void) {
-    const botao = document.createElement("input");
-    botao.type = "button";
-    botao.value = desc;
-    botao.onclick = func;
-    return botao;
-  }*/
+  function obterEndereco(v: string){
+    let tipo = v.slice(0, v.search("\\."));
+    let midia = v.slice(v.search("\\.")+1);
+    let arraysDeArquivos = {imagensDeFundo,personagens,musicas,sons};
+    let array = tipo as keyof typeof arraysDeArquivos;
+    return arraysDeArquivos[array][midia];
+  }
 
   function menuNovoJogo(){
     //tocarBip();
@@ -95,45 +84,51 @@ export default function TelaInicial() {
   function menuOpcoes(){
     //tocarBip();
     sistema?.despachar({tipo: acoes.exibirTelaDeOpcoes, opcao: true});
-    //sistema?.despachar({tipo: acoes.mudarTela, string: "opções"});
   }
 
   function menuArquivos(){
     //tocarBip();
     sistema?.despachar({tipo: acoes.exibirTelaDeArquivos, opcao: true});
-    //sistema?.despachar({tipo: acoes.mudarMusica, endereco: ""});
-    //sistema?.despachar({tipo: acoes.mudarTela, string: "opções"});
   }
   
-  //function tocarBip(){
-  //  sistema?.despachar({tipo: acoes.tocarSom, endereco: sons.bip});
-  //}
-
   return (
     <>
-    {/*{!sistema?.estado.exibindoTelaDeOpcoes &&*/}
-      <div id="menu" style={{
-          position: "absolute",
-          width: "100%",
-          height: "33%",
-          bottom: "6%",
-          display: "grid",
-          grid: "auto/max-content",
-          placeContent: "space-evenly center",
-          opacity: 0,
-        }}>
-        {/*<button onClick={menuNovoJogo}>Novo Jogo</button>
-        <button onClick={menuContinuar}>Continuar</button>
-        <button onClick={menuOpcoes}>Opções</button>*/}
-        <Botao nome="Novo jogo" func={menuNovoJogo}/>
-        <Botao nome="Testar roteiro" func={menuTestarRoteiro}/>
-        <Botao nome="Opções" func={menuOpcoes}/>
-        <Botao nome="Gerenciar arquivos" func={menuArquivos}/>
-        {/*<Botao nome="Novo Jogo" func={menuNovoJogo} style={{opacity: 0}}/>
-        <Botao nome="Continuar" func={menuContinuar} style={{opacity: 0}}/>
-        <Botao nome="Opções" func={menuOpcoes} style={{opacity: 0}}/>*/}
-      </div>
-    {/*}*/}
+    <div id="menu" style={{
+        position: "absolute",
+        width: "100%",
+        height: "33%",
+        bottom: "6%",
+        display: "grid",
+        grid: "auto/max-content",
+        placeContent: "space-evenly center",
+        opacity: 0,
+      }}
+    >
+      <Botao nome="Novo jogo" func={menuNovoJogo}/>
+
+      <input type="file"
+        accept=".json"
+        id="adicionar roteiro"
+        style={{display: "none"}}
+        onChange={()=>{
+          let arqElem = document.getElementById("adicionar roteiro");
+          if(arqElem && arqElem instanceof HTMLInputElement && arqElem.files)
+            definirArqRoteiro(arqElem.files[0]);
+        }}
+      />
+      <Botao nome="Importar roteiro e executar"
+        func={()=>{
+          let arqElem = document.getElementById("adicionar roteiro");
+          if(arqElem)
+            arqElem.click();
+        }}
+        title="Antes de importar o roteiro, certifique-se de ter importado todos os arquivos que são utilizados nele."
+      />
+
+      <Botao nome="Opções" func={menuOpcoes}/>
+
+      <Botao nome="Gerenciar arquivos" func={menuArquivos}/>
+    </div>
     </>
   )
 }
